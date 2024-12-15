9a10b91cf73e97548a32a1362d99d66094d5f73f
giacomodib <157379848+giacomodib@users.noreply.github.com>
2024-12-15 08:04:39 +0100

    Changed exchange type to topic.

--- client/NetCoreClient/Protocols/Amqp.cs
+++ client/NetCoreClient/Protocols/Amqp.cs
@@ -1,5 +1,6 @@
 ﻿using RabbitMQ.Client;
 using System;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace NetCoreClient.Protocols
     public class Amqp : IProtocolInterface
     {
         private readonly string _hostname;
-        private const string QUEUE_NAME = "sensor_data_queue";
+        private const string EXCHANGE_NAME = "sensor_data_exchange";
         private IConnection _connection;
         private IChannel _channel;
 
@@ -20,25 +21,26 @@ namespace NetCoreClient.Protocols
         public async Task InitializeAmqpClient()
         {
             var factory = new ConnectionFactory() { Uri = new Uri(_hostname) };
-            _connection = await factory.CreateConnectionAsync(); 
-            _channel = await _connection.CreateChannelAsync();  
-
-            _channel.QueueDeclareAsync(QUEUE_NAME, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            _connection = await factory.CreateConnectionAsync();
+            _channel = await _connection.CreateChannelAsync();
+            await _channel.ExchangeDeclareAsync(EXCHANGE_NAME, ExchangeType.Topic, durable: false, autoDelete: false);
         }
 
         public async Task Send(string data, string sensor)
         {
+            string routingKey = $"water_coolers.123.sensor.{sensor}";
+
             var body = Encoding.UTF8.GetBytes(data);
 
-            _channel.BasicPublishAsync("", QUEUE_NAME, body);
+            await _channel.BasicPublishAsync(exchange: EXCHANGE_NAME, routingKey: routingKey, body: body);
 
             Console.WriteLine($" [x] Sent {sensor}: {data}");
         }
 
         public async Task Close()
         {
-            await _channel.CloseAsync();  
-            await _connection.CloseAsync();  
+            await _channel.CloseAsync();
+            await _connection.CloseAsync();
         }
     }
 }
--- server/amqp.js
+++ server/amqp.js
@@ -5,24 +5,34 @@ async function start() {
     const connection = await amqp.connect(process.env.AMQP_URL);
     const channel = await connection.createChannel();
 
-    const queue = 'sensor_data_queue';
-    await channel.assertQueue(queue, { durable: false });
+    const exchangeName = 'sensor_data_exchange';
+    await channel.assertExchange(exchangeName, 'topic', { durable: false });
 
-    console.log("Waiting for messages in %s", queue);
+    const queue = await channel.assertQueue('', { exclusive: true });
 
-    channel.consume(queue, async (msg) => {
+    console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+    const routingKeyPattern = 'water_coolers.*.sensor.#';
+
+    await channel.bindQueue(queue.queue, exchangeName, routingKeyPattern);
+
+    channel.consume(queue.queue, async (msg) => {
         if (msg !== null) {
+            const routingKey = msg.fields.routingKey;
             const sensorData = JSON.parse(msg.content.toString());
             const { Name, Value } = sensorData;
-            const coolerId = 123;
+
+            const parts = routingKey.split('.');
+            const coolerId = parts[1]; // ID della casetta
 
             try {
                 await insertWaterCoolerData(coolerId, Name, Value);
-                console.log("Data inserted:", sensorData);
+                console.log("Routing key:", msg.fields.routingKey, "- Data inserted: ", sensorData);
 
                 channel.ack(msg);
             } catch (err) {
                 console.error('Failed to insert data into InfluxDB', err);
+
                 channel.nack(msg);
             }
         }
